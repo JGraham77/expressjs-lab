7907df2d154919b0f505ecc1a0d041842fa82819
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-04-25 17:48:51 -0500

    completed lab

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 let app = express();
+let dataPath = path.join(__dirname, '../request.json');
+let arr = [];
 
 // app.get('/', (req, res) => {
 //     res.send('Hello from the web server side...');
@@ -15,6 +17,20 @@ app.use((req, res, next) => {
     console.log(req.url);
     next();
 });
+
+app.post('/formsubmissions', (req, res) => {
+    let obj = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+    arr = [...arr, obj];
+    fs.writeFile(dataPath, JSON.stringify(arr), (err) => {
+        if (err) console.log(err);
+    });
+
+    res.send(arr);
+});
+
 
 app.use(express.static(path.join(__dirname, '../public')));
 
